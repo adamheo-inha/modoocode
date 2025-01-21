e8be51039565b7216705fc5248e0e33f5a666588
adamheo-inha <adamheo@inha.edu>
2025-01-21 12:24:14 +0900

    1.0.0

--- cpp/cpp04/p0101.cpp
+++ cpp/cpp04/p0101.cpp
@@ -2,108 +2,113 @@
 
 class Date {
     private:
+    // 현재 년 월 일
     int year_;
     int month_;  // 1 부터 12 까지.
-    int day_;    // 1 부터 31 까지.
+    int day_;    // 1 부터 28,30,31 까지.
     
  public:
-    int Month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int monthcheck = 0;
-    //함수 내부 초기화 
+    int Month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};//1월부터 12월까지 마지막날 배열
+    int lastday = 0;  //달의 마지막날 28,30,31
+
+// 그 달의 마지막 날
+    void LastDayOfMonth(int month_) {
+        lastday = Month[month_ - 1];
+    }//LastDayOfMonth
+    
+// 초기 날짜 설정
     void SetDate(int year = 0, int month = 0, int day = 0) {
         year_ = year;
         month_ = month;
         day_ = day;
-        int maxday_ = 0;
+
         std::cout << "원하시는 년도를 숫자만 입력하세요. ";
         std::cin >> year_;
         std::cout << "원하시는 달을 숫자만 입력하세요. ";
         std::cin >> month_;
+        // 1~12 값이 들어올때까지 반복
         while (1) {
-            // 달이 1부터 12사이에 들어왔는지 확인
+            // 1부터 12사이에 들어왔는지 확인
             if(1 <= month_ && month_ <= 12) {
+                LastDayOfMonth(month_);
                 break;
                 //조건이 성립해서 while exit
-            // 달이 1부터 12이외에 입력값이 들어와 재입력 요청과 값 입력과 수정
+            // 1부터 12이외에 입력값이 들어와 재입력 요청과 값 입력과 수정
             } else {
                 std::cout << "달을 다시 입력해줘요." << std::endl;
                 std::cin >> month_;
             }
-        }//while 
+        }//while ()
 
 
-        monthcheck = Month[month_ - 1];
-        std::cout << monthcheck << " " << month_ << std::endl; ///////////////////////////////////////////////        
-
+        
         std::cout << "원하시는 날짜를 숫자만 입력하세요. ";
         std::cin >> day_;
-        // 해당 달에 넘는 날짜가 들어왔는지 확인
-        // 들어왔으면 while문을 탈출하고 넘었으면 재입력을 요청
-        while (1) {
-            // 입력될 달에 해당하는지 확인
-            if (monthcheck == 31) {
+        // 월 입력에서 얻은 마지막날 조건확인
+        char notoverinputagain[] = "일을 넘지마세요. 다시 입력해주세요.";
+        if (lastday == 31) {
+            while(1) {
                 // 달에 맞는 날안에 들어오면 while문 탈출
                 if (1 <= day_ && day_ <= 31) {
                     break;
                 // 들어오지 못하면 넘었다는 문구 출력과 최대날 변수에 입력
                 } else {
-                    std::cout << "31일을 넘지마세요." << std::endl;
-                    maxday_ = 31;
+                    std::cout << lastday << notoverinputagain << std::endl;
+                    std::cin >> day_;
                 }
-            } else if (monthcheck == 30) {
+            }    
+        } else if (lastday == 30) {
+            while(1) {
                 if (1 <= day_ && day_ <= 30) {
                     break;
                 } else {                    
-                    std::cout << "30일을 넘지마세요." << std::endl;
-                    maxday_ = 30;                                                                                    
+                    std::cout << lastday << notoverinputagain << std::endl;
+                    std::cin >> day_;                                                                                    
                 }
-            } else if (monthcheck == 28) {
+            }
+        } else if (lastday == 28) {
+            while(1) {
                 if (1 <= day_ && day_ <= 28) {
                     break;
                 } else {                    
-                    std::cout << "28일을 넘지마세요." << std::endl;
-                    maxday_ = 28;
+                    std::cout << lastday << notoverinputagain << std::endl;
+                    std::cin >> day_;
                 }
             }
-            // 재입력 요청
-            std::cout << maxday_ << "일을 넘지 않는 날짜를 숫자만 입력하세요. ";
-            std::cin >> day_;
-        }//while
+        }//if()
     }//SetDate()
 
 
     // 날짜 더하기
     void AddDay(int inc) {
-        day_ += inc;
-        if (monthcheck == 31) {
-            while(day_ > 31) {
-                day_ -= 31;
+        for (int i = 0; i < inc; i++) {
+            day_++;
+            if (lastday == 31 && day_ == 32) {
+                day_ = 1;
+                AddMonth(1);
+            } else if (lastday == 30 && day_ == 31) {
+                day_ = 1;
+                AddMonth(1);
+            } else if (lastday == 28 && day_ == 29) {
+                day_ = 1;
                 AddMonth(1);
             }
-        } else if (monthcheck == 30) {
-            while(day_ > 30) {
-                day_ -= 30;
-                AddMonth(1);
-            }
-        } else if (monthcheck == 28) {
-            while(day_ > 28) {
-                day_ -= 28;
-                AddMonth(1);
-            }
-        } else {
-            std::cout << "월정 정정 해주세요." << std::endl;
         }
-        std::cout << "3 " << monthcheck << " " << month_ << std::endl;////////////////////////////////////////////
     }//AddDay()
 
     void AddMonth(int inc) {
-        month_ += inc;
-        if(month_ > 11) {
-            month_ -= 12;
-            AddYear(1);
+        for (int i = 0; i < inc; i++) {
+            month_++;
+            LastDayOfMonth(month_);
+            if(month_ == 13) {
+                month_ = 1;
+                AddYear(1);
+            }
         }
-        monthcheck = Month[month_ - 1];
-        std::cout << "AddMonth 함수후" << monthcheck << " " << month_ << std::endl;////////////////////////////////////////////
+        
+        if (day_ > lastday) {
+            day_ = lastday;
+        }
     }//AddMonth()
 
     void AddYear(int inc){
@@ -113,7 +118,6 @@ class Date {
     //날짜 출력
     void ShowDate() {
         std::cout << year_ << "년 " << month_ << "월 " << day_ << "일" << std::endl;
-        std::cout << monthcheck << std::endl;
     }//ShowDate()
 };
 
@@ -135,19 +139,19 @@ int main() {
                 std::cout << "몇 년 추가 하시겠습니까? : ";
                 std::cin >> inc;
                 date.AddYear(inc);
-                date.ShowDate();
+                //date.ShowDate();
                 break;
             case 2:
                 std::cout << "몇 달 추가 하시겠습니까? : ";
                 std::cin >> inc;
                 date.AddMonth(inc);
-                date.ShowDate();
+                //date.ShowDate();
                 break;
             case 3:
                 std::cout << "몇 일 추가 하시겠습니까? : ";
                 std::cin >> inc;
                 date.AddDay(inc);
-                date.ShowDate();
+                //date.ShowDate();
                 break;
             case 4:
                  date.ShowDate();
@@ -160,8 +164,6 @@ int main() {
             break;
         }
     }
-
-
 
     return 0;
 }
